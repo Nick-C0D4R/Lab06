3a22adfd6bcf977a47207fb3701cb2e3f6267a37
Mykola Kovshar <knikolay20031230@gmail.com>
2022-10-29 19:19:31 +0300

    Add MyList<T> project

---
+++ MyList/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class MyList<T>
+    {
+        private T[] list;
+
+        public MyList() => list = new T[0];
+
+        public T this[int index]
+        {
+            get { return list[index]; }
+            set { list[index] = value; }
+        }
+
+        public void Add(T value)
+        {
+            Array.Resize(ref list, Count + 1);
+            list[Count - 1] = value;
+        }
+
+        public int Count => list.Length; 
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> list = new MyList<string>();
+            list.Add("3 apple");
+            list.Add("10");
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
